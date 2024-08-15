7e7846e2603af3da100ef25528a8ceb43f585971
EtoNeAnanasbI95 <ananasovii.yashik@gmail.com>
2024-08-15 19:56:15 +0300

    prepair for normal API proj structure
    add future endpoints

---
+++ internal/handlers/users.go
@@ -0,0 +1,19 @@
+package handlers
+
+import "net/http"
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Deleteuser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+
+}
