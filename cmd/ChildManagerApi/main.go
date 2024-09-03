63df2c0236ea726c1088077da3dbe60ee9f7fcdc
EtoNeAnanasbI95 <ananasovii.yashik@gmail.com>
2024-09-03 23:50:12 +0300

    Remove trash

--- cmd/ChildManagerApi/main.go
+++ cmd/ChildManagerApi/main.go
@@ -1,23 +1,8 @@
 package main
 
-import (
-	"fmt"
-
-    "github.com/EtoNeAnanasbI95/ChildManagerApi/internal/handlers"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/v5/middleware"
-)
+import "github.com/EtoNeAnanasbI95/ChildManagerApi/internal/config"
 
 func main() {
-    fmt.Println("Server is running")
-    
-    r := chi.NewRouter()
-    
-    r.Use(middleware.Logger) // add Logger
-    r.Use(middleware.Recoverer) // add Recoverer that can recover logs from panic and will give a 500 code if it possible
-    
-    r.Get("/users", handlers.GetUsers)
-    r.Put("/users", handlers.UpdateUser)
-    r.Post("/users", handlers.CreateUser)
-    r.Delete("/users", handlers.Deleteuser)
+    cfg := config.MustLoadConfig()
+    _ = cfg
 }
